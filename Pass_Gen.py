c161eaa9a47651f1f05100403bcd8d66bbe08c72
BlueSynAck <71300144+AlbertL7@users.noreply.github.com>
2024-04-05 15:37:08 -0700

    Update Pass_Gen.py
    For use with MacOS

--- Pass_Gen.py
+++ Pass_Gen.py
@@ -1,39 +1,83 @@
 import secrets
 import string
-import sys
+import subprocess
+import getpass
+from datetime import datetime
 
-print('####Password Generator####\n')
+"""
+How to Use
+1. Have a text file you want to encrypt and keep your passwords in. I called it Vault
+2. Open Disk Utility and create a new image
+    - Disk Utility > got to File > New Image > Blank Image
+3. Configure Disk Image
+    - Save as: Give Disk Image a name
+    - Name: Name that will appear when image is mounted, I called mine Vault, same as the file I want to encrypt
+    - Size: Choose a small size like 10MB
+    - Format: Choose Mac OS Extended (Journaled)
+    - Encryption: Choose AES 128 or 256 and set your password
+        NOTE: set password the same as your sudo password
+    - Partitions: Choose Single Partition - Apple Partition Map
+    - Image Format: Choose "read/write" disk image
+4. Create and mount the Disk image. 
+5. Once the Disk image is mounted move the file onto the Disk Image
+6. Eject the Disk image and that file is now encrypted.
+7. You must run this program in the terminal, it requires an interactive terminal session for sudo authorization.
+    - an easy way to do this is to bring up spotlight and type terminal. Click ther terminal application and run this using python3.
+"""
 
-# define the alphabet
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
+def passgen():
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation # or you can use the following to customize the exact you want to use special characters: "!@#$%^&*()-_=+[{]}|;:',<.>/?"
+    alphabet = letters + digits + special_chars
+    
+    
+    account = input('Account Name / website: ')
+    username = input('Username / Email: ')
+    extra = input('Amplifying Information / Pin: ')
+    usr_pwd = input('Would you like to create your own password? (y / n): ').lower()
+    while True:
+        if usr_pwd == 'y':
+            pwd = input("--> ")
+            break
+        elif usr_pwd == 'n':
+            pwd_length = int(input('\nEnter the number length of the password: '))
+            pwd = ''.join(secrets.choice(alphabet) for i in range(pwd_length))
+            print('Generated Password:', pwd)
+            redo = input("Do you want to generate a different password? (y / n) ").lower()
+            if redo != 'y':
+                break
+        else:
+            print("Error --> Pleae choose 'y' or 'n': ")
+    time_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-alphabet = letters + digits + special_chars
+    print(f'\nAccount: {account}\nPASSWORD: {pwd}\nUSERNAME: {username}\nAMPLIFYING INFO: {extra}\nDate: {time_date}')
+    
+    return account, username, extra, pwd, time_date
 
-# Account name
-account = input('Name of Account / website = ')
+def is_volume_mounted(volume_path):
+    result = subprocess.run(["df"], capture_output=True, text=True)
+    return volume_path in result.stdout
 
-# Username
-username = input('Username / Email = ')
+def mount_disk(image_path, volume_path):
+    if is_volume_mounted(volume_path):
+        print(f"Volume at '{volume_path}' is already mounted. Writing Account info to Vault.....")
+        return
+    command = f"sudo hdiutil attach '{image_path}' -stdinpass"
+    subprocess.run(command, shell=True, check=True)
 
-# Extra info
+def append_to_file(append_to_file_path, account, username, extra, pwd, time_date):
+    with open(append_to_file_path, 'a') as file:
+        print(f'\n\nAccount: {account}\nPASSWORD: {pwd}\nYour USERNAME: {username}\nAMPLIFYING INFO: {extra}\nDate: {time_date}', file=file)
+        
+    print(f"Account information written to {append_to_file_path}")
+    print("\n####Password Generated####\n")
 
-extra = input('Amplifying Information / Pin = ')
+print('\n####Password Generator####\n')
+image_path = 'Change this' # Example: '/Users/username/Desktop/vault.dmg'
+volume_path = 'Change this'  # This is the mount point of the disk image Example: '/Volumes/Vault'
+append_to_file_path = f'{volume_path}/change this' # Exmaple f'{volume_path}/Vault'
 
-# fix password length
-pwd_length = int(input('\nEnter the number length of the password\n'))
-
-# generate a password string
-pwd = ''
-for i in range(pwd_length):
-  pwd += ''.join(secrets.choice(alphabet))
-
-print('\nYour PASSWORD =',pwd, 'your USERNAME =', username, 'your AMPLIFYING INFO =', extra, 'for', account)
-
-original_stdout = sys.stdout
-
-with open('passwords.txt', 'a') as f:
-  sys.stdout = f 
-  str(print('\n', account, '|', username, '|', pwd, '|', extra,'\n'))
-  sys.stdout = original_stdout
+account, username, extra, pwd, time_date = passgen()
+mount_disk(image_path, volume_path)
+append_to_file(append_to_file_path, account, username, extra, pwd, time_date)
